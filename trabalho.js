be736c0da7b397b66b298af68652e76bf651b7a8
Victor2002H <victorbes20@gmail.com>
2025-04-11 21:53:09 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,5 @@
+function carConteudo(){
+    document.getElementById("conteudotexto").innerHTML = "<p>Olá, seja muito bem-vindo! É um prazer recebê-lo por aqui — sinta-se à vontade para explorar e conhecer mais sobre meu trabalho!</p>";
+}
+
+
